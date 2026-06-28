ac9b87ecec27f4b21c4b1413482c2f309d1237d9
Aryan892-C <aarora99990@gmail.com>
2026-06-28 20:09:48 +0530

    message

--- index.js
+++ index.js
@@ -1,238 +1,360 @@
 let mainFrame = document.getElementById("mainframe");
-mainFrame.classList="flex flex-col border-3 border-green-400 "
 
-let tasksArray=JSON.parse(localStorage.getItem("TASKS"));
-if(tasksArray == null)
-{
-    tasksArray = [];
-}
+let buttonDiv = document.createElement("div");
+buttonDiv.classList="flex-col m-5 border-2 border-solid w-80 h-20";
+mainFrame.appendChild(buttonDiv);
 
-function saveToLocalStorage(task)
-{
-    tasksArray.push(task);
-    localStorage.setItem("TASKS",JSON.stringify(tasksArray));
-}
 let addBtn = document.createElement('button');
 addBtn.innerText = "ADD";
-addBtn.classList = "bg-red border-2 border-solid";
+addBtn.classList = "bg-blue-400 border-2 border-bg-blue-800 w-20 h-10 m-3";
 
-let showBtn=document.createElement("div");
-showBtn.innerText="SHOW";
-showBtn.classList="bg-red-400 border-2 w-5 h-4 m-3"
+let showBtn = document.createElement('button');
+showBtn.innerText = "SHOW";
+showBtn.classList = "bg-blue-400 border-2 border-blue-800 w-20 h-10 m-3";
+
+buttonDiv.appendChild(addBtn);
+buttonDiv.appendChild(showBtn);
 
 let frame1 = document.createElement("div");
+frame1.classList = "m-5 border-2 border-solid flex flex-row";
 frame1.style.display = "none";
-frame1.classList = "flex flex-col border-2 border-solid w-100 h-100 mt-3 bg-red-300";
-frame1.style.display = "none";
 
-let input_box = document.createElement("input");
-input_box.id = "input_data";
-input_box.type = "text";
-input_box.placeholder = "Enter the task.";
-input_box.classList = "border-2 border-solid mt-2 bg-green-300";
+let inputBox = document.createElement("input");
+inputBox.placeholder = "enter the task";
+inputBox.classList = "bg-red-300 m-3 border-2 border-bg-green-300";
 
-let task_description = document.createElement("textarea");
-task_description.placeholder = "Task description.";
-task_description.rows = 4;
-task_description.classList = "border-2 border-solid w-90 mt-2";
+let descriptionBox = document.createElement("textarea");
+descriptionBox.placeholder = "type description of task";
+descriptionBox.classList = " bg-green-500 m-3 border-2 border-solid";
 
-let addItem = document.createElement("button");
+let addItem = document.createElement('button');
 addItem.innerText = "ADD ITEM";
-addItem.classList = "border-2 border-solid mt-2";
-mainFrame.appendChild(frame1);
-frame1.appendChild(input_box);
-frame1.appendChild(task_description);
+addItem.classList = "bg-blue-400 border-2 border-blue-800 w-20 h-10 m-3";
+
+frame1.appendChild(inputBox);
+frame1.appendChild(descriptionBox);
 frame1.appendChild(addItem);
 
-let frame2 = document.createElement('div');
+let frame2 = document.createElement("div");
+frame2.classList = "m-5 border-2 border-solid bg-blue-200";
 frame2.style.display = "none";
-frame2.classList = "border-2 border-solid h-150 w-200 mt-3 bg-blue-200"
-//ShowAndHideButton.addEventListener("click",showHideTasks());
-frame2.style.display="none";
 
-function task_window()
+let frame2Btn = document.createElement("div");
+frame2Btn.classList = "m-5 border-2 border- flex justify-center";
+
+let search = document.createElement("button");
+search.innerText = "search";
+search.classList = "bg-blue-400 border-2 border-blue-800 w-20 h-10 m-3";
+
+let searchInput = document.createElement("input");
+searchInput.placeholder = "search the task";
+searchInput.classList = "bg-blue-400 m-3 border-2 border-solid";
+
+frame2Btn.appendChild(searchInput);
+frame2Btn.appendChild(search);
+
+let taskContainer = document.createElement("div");
+taskContainer.classList = "m-3 border-2 border-solid";
+
+
+let msgDiv = document.createElement("p");
+msgDiv.innerText = "There is no task";
+msgDiv.classList = "text-pink-500 m-5";
+
+frame2.appendChild(frame2Btn);
+frame2.appendChild(taskContainer);
+frame2.appendChild(msgDiv);
+
+addBtn.addEventListener("click", ()=>{
+    mainFrame.appendChild(frame1);
+    frame1.style.display = "block";
+    frame2.style.display = "none";
+    if (showBtn.innerText == "HIDE") 
+    {
+        showBtn.innerText = "SHOW";
+    }
+});
+
+function showHide() 
 {
-    if(frame2.style.display == "none")
+    if (frame2.style.display == "none") 
     {
         frame2.style.display = "block";
         frame1.style.display = "none";
         showBtn.innerText = "HIDE";
-    }
+    } 
     else 
     {
-        frame1.style.display = "block";
         frame2.style.display = "none";
+        frame1.style.display = "block";
         showBtn.innerText = "SHOW";
-
     }
 }
 
-addBtn.addEventListener('click', ()=>
-{   
-    mainFrame.appendChild(frame1);
-    frame1.style.display = "block";
-    frame2.style.display = "none";
-    if(showBtn.innerText == "HIDE")
-    {
-        showBtn.innerText = "SHOW";
-    }
-});   
-    
-    showBtn.addEventListener("click",task_window());
+showBtn.addEventListener("click", showHide);
 
-    addItem.addEventListener('click', ()=>
-    {
-    task_window();
-    let taskDiv = document.createElement("div");
-    taskDiv.classList = " flex flex-col border-2 border-solid bg-pink-200 h-50";
-    
-    let data = input_box.value;
-    let description_data = task_description.value;
-    if(data == "" || description_data == "")
+
+let tasksArray = JSON.parse(localStorage.getItem("TASKS"));
+if (tasksArray == null) {
+    tasksArray = [];
+}
+else{
+    taskFromLocalStorage(tasksArray);
+}
+
+addItem.addEventListener("click", ()=>{
+    let data = inputBox.value;
+    let description_data = descriptionBox.value;
+    let todayDate = new Date().toLocaleString();
+
+    if (data == "" || description_data == "") 
     {
         alert("task or description can't be empty");
         return;
     }
-    let taskName = document.createElement('h1');
-    taskName.textContent = data;
-    taskName.classList = "text-xl";
 
-    let description = document.createElement("div");
-    description.innerText = description_data;
-    description.classList = "bg-green-300 break-all truncate";
+    showHide();
+
+    let task = {
+        taskName: data,
+        taskDescription: description_data,
+        taskDate: todayDate,
+        taskId: new Date().getTime(),
+        taskStatus: false
+    };
+
+    tasksArray.push(task);
+    tasksArray.sort((task1,task2)=>task2.taskId-task1.taskId);
+    saveToLocalStorage(tasksArray);
+
+    msgDiv.style.display = "none";
+
+    createTaskDiv(task, true);
+
+    inputBox.value = "";
+    descriptionBox.value = "";
+});
+
+function saveToLocalStorage(tasksArray) 
+{
+    /*tasksArray.push(task);
+    console.log(tasksArray[0].taskId);
+    console.log(tasksArray[0].taskDescription);*/
+    localStorage.setItem("TASKS", JSON.stringify(tasksArray));
+}
+
+function taskFromLocalStorage(tasksArray) 
+{
+    if (tasksArray.length > 0) 
+    {
+        msgDiv.style.display = "none";
+    }
+    tasksArray.sort((task1, task2) => task2.taskId - task1.taskId);
+    for (let i = 0; i < tasksArray.length; i++) 
+    {
+        createTaskDiv(tasksArray[i],false);
+    }
+}
+
+function createTaskDiv(task, flag)
+{
+    let taskDiv = document.createElement("div");
+    taskDiv.classList = " border-2 border-[#659287] bg-[#B1D3B9] m-3";
+
+    let infoDiv = document.createElement("div");
+    infoDiv.classList = "flex flex-col ";
+
+    let taskName = document.createElement('h1');
+    taskName.textContent = task.taskName;
+    taskName.classList = "text-xl m-2";
+
+    let description = document.createElement("p");
+    description.innerText = task.taskDescription;
+    description.classList = " border-2 m-2 bg-[#E6F2DD]  w-80 break-all truncate";
 
     let readmoreless = document.createElement('button');
     readmoreless.innerText = "readmore";
     readmoreless.style.color = "blue";
-    readmoreless.addEventListener('click',()=>
-    {
+    readmoreless.classList = "self-start m-2";
+    readmoreless.addEventListener('click', ()=>{
         description.classList.toggle("truncate");
-
         if(readmoreless.innerText == "readmore")
         {
-             readmoreless.innerText = "readless";
+            readmoreless.innerText = "readless";
         }
-        else if(readmoreless.innerText == "readless")
+        else
         {
             readmoreless.innerText = "readmore";
         }
-       
     });
 
-    let editBtn=document.createElement("button");
-    editBtn.innerText="✏️";
-    editBtn.classList="basis-1/5 bg-pink-500";
-    taskDiv.appendChild(editBtn);
-    editBtn.addEventListener("click",()=>
-    {
-        frame1.style.display="block";
-        frame2.style.display="none";
-    });
+    let date = document.createElement("p");
+    date.innerText = task.taskDate;
+    date.classList = "m-2";
 
-    let removeBtn = document.createElement("button");
-    removeBtn.innerText="🗑️";
-    removeBtn.classList=" bg-red-400 basis-1/4";
-    taskDiv.appendChild(removeBtn);
-    removeBtn.addEventListener("click",()=>
+    infoDiv.appendChild(taskName);
+    infoDiv.appendChild(description);
+    if (task.taskDescription.length >= 200) 
     {
-        taskDiv.remove();
-    });
-    let doneBtn = document.createElement("button");
-    doneBtn.innerText="✅";
-    doneBtn.classList="basis-1/4";
-    taskDiv.appendChild(doneBtn);
-    doneBtn.addEventListener("click",()=>
-    {
-        taskDiv.classList.toggle("line-through");
-    });
-    taskDiv.appendChild(taskName);
-    taskDiv.appendChild(description);
-    if(description_data.length >= 200)
-    {
-         taskDiv.appendChild(readmoreless);
+        infoDiv.appendChild(readmoreless);
     }
-    taskDiv.appendChild(editBtn);
-    taskDiv.appendChild(removeBtn);
-    taskDiv.appendChild(doneBtn);
+    infoDiv.appendChild(date);
 
-    saveToLocalStorage(task);
 
-    input_box.value = "";
-    task_description.value = "";
+    let taskBtnDiv = document.createElement("div");
+    taskBtnDiv.classList = "m-3"
 
-    frame2.appendChild(taskDiv);
+    let doneBtn = document.createElement("button");
+    doneBtn.innerText = "DONE";
+    doneBtn.classList = "bg-blue-400 border-2 border-blue-800 w-20 h-10 m-3"
 
-    
-});
-function createTaskDivs(tasksArray)
-{
-    for(let i=0;i<tasksArray.length;i++)
-    {
-        let taskDiv = document.createElement("div");
-        taskDiv.classList = " flex flex-col border-2 border-solid bg-pink-200 h-50";
-        let taskName = document.createElement('h1');
-        taskName.textContent = data;
-        taskName.classList = "text-xl";
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "EDIT";
+    editBtn.classList = "bg-blue-400 border-2 border-blue-800 w-20 h-10 m-3";
 
-        let description = document.createElement("div");
-        description.innerText = description_data;
-        description.classList = "bg-green-300 break-all truncate";
+    let saveBtn = document.createElement("button");
+    saveBtn.innerText = "SAVE";
+    saveBtn.classList = "bg-blue-400 border-2 border-blue-800 w-20 h-10 m-3";
+    saveBtn.style.display = "none";
 
-        let readmoreless = document.createElement('button');
-        readmoreless.innerText = "readmore";
-        readmoreless.style.color = "blue";
-        readmoreless.addEventListener('click',()=>
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "DELETE";
+    deleteBtn.classList = "bg-blue-400 border-2 border-blue-800 w-20 h-10 m-3"
+
+
+    taskBtnDiv.appendChild(doneBtn);
+    taskBtnDiv.appendChild(editBtn);
+    taskBtnDiv.appendChild(saveBtn);
+    taskBtnDiv.appendChild(deleteBtn);
+
+    doneBtn.addEventListener("click", ()=>{
+
+        for(let i = 0; i <tasksArray.length; i++)
         {
-            description.classList.toggle("truncate");
-
-            if(readmoreless.innerText == "readmore")
+            if(tasksArray[i].taskId == task.taskId)
             {
-                readmoreless.innerText = "readless";
+                if(tasksArray[i].taskStatus == true)
+                {
+                    tasksArray[i].taskStatus = false;
+                }
+                else
+                {
+                    tasksArray[i].taskStatus = true;
+                }
             }
-            else if(readmoreless.innerText == "readless")
-            {
-                readmoreless.innerText = "readmore";
-            }
-        });
-
-        let editBtn=document.createElement("button");
-        editBtn.innerText="✏️";
-        editBtn.classList="basis-1/5 bg-pink-500";
-        taskDiv.appendChild(editBtn);
-        editBtn.addEventListener("click",()=>
-        {
-            frame1.style.display="block";
-            frame2.style.display="none";
-        });
-
-        let removeBtn = document.createElement("button");
-        removeBtn.innerText="🗑️";
-        removeBtn.classList=" bg-red-400 basis-1/4";
-        taskDiv.appendChild(removeBtn);
-        removeBtn.addEventListener("click",()=>
-        {
-         taskDiv.remove();
-        });
-        let doneBtn = document.createElement("button");
-        doneBtn.innerText="✅";
-        doneBtn.classList="basis-1/4";
-        taskDiv.appendChild(doneBtn);
-        doneBtn.addEventListener("click",()=>
-        {
-            taskDiv.classList.toggle("line-through");
-        });
-        taskDiv.appendChild(taskName);
-        taskDiv.appendChild(description);
-        if(description_data.length >= 200)
-        {
-            taskDiv.appendChild(readmoreless);
         }
-        taskDiv.appendChild(editBtn);
-        taskDiv.appendChild(removeBtn);
-        taskDiv.appendChild(doneBtn);
+
+        saveToLocalStorage(tasksArray);
+        if(task.taskStatus == true)
+        {
+            doneBtn.innerText = "UNDONE";
+            infoDiv.classList = "bg-yellow-300"
+        }
+        else
+        {
+            doneBtn.innerText = "DONE";
+            infoDiv.classList.remove("bg-yellow-300");
+        }
+    })
+    if(task.taskStatus == true)
+    {
+        doneBtn.innerText = "UNDONE";
+        infoDiv.classList = "bg-yellow-300";
+    }
+    else
+    {
+        doneBtn.innerText = "DONE";
+        infoDiv.classList.remove("bg-yellow-300");
+    }
+
+    let newInput = document.createElement("input");
+    let newDescription = document.createElement("textarea");
+    editBtn.addEventListener("click",()=>{
+        saveBtn.style.display = "inline-block";
+        editBtn.style.display = "none";
+        infoDiv.innerHTML = "";
+
+        newInput.value = task.taskName;
+        newInput.classList = "border-2";
+        
+        newDescription.value = task.taskDescription;
+        newDescription.classList = "border-2";
+
+        infoDiv.appendChild(newInput);
+        infoDiv.appendChild(newDescription);
+    })
+
+    saveBtn.addEventListener("click", ()=>{
+        task.taskName = newInput.value;
+        task.taskDescription = newDescription.value;
+        task.taskDate = new Date().toLocaleString();
+
+        let index = tasksArray.findIndex((t) => t.taskId == task.taskId);
+        tasksArray[index] = task;
+        localStorage.setItem("TASKS", JSON.stringify(tasksArray));
+
+        infoDiv.innerHTML = "";
+        taskName.textContent = task.taskName;
+        description.innerText = task.taskDescription;
+        date.innerText = task.taskDate;
+        infoDiv.appendChild(taskName);
+        infoDiv.appendChild(description);
+        if (task.taskDescription.length >= 50) 
+            {
+                infoDiv.appendChild(readmoreless);
+            }
+        infoDiv.appendChild(date);
+
+        saveBtn.style.display = "none";
+        editBtn.style.display = "inline-block";
+    })
+
+    deleteBtn.addEventListener("click", ()=>{
+        tasksArray = tasksArray.filter((t) => t.taskId != task.taskId);
+        localStorage.setItem("TASKS", JSON.stringify(tasksArray));
+        taskDiv.remove();
+    })
+ 
+    taskDiv.appendChild(infoDiv);
+    taskDiv.appendChild(taskBtnDiv);
+    
+    
+
+    if(flag)
+    {
+        taskContainer.prepend(taskDiv)
+    }
+    else
+    {
+        taskContainer.appendChild(taskDiv);
     }
 }
-mainFrame.appendChild(addBtn);
-mainFrame.appendChild(showBtn);
-createTaskDivs(tasksArray);
+
+search.addEventListener("click", ()=>{
+    let searchValue = searchInput.value.toLocaleLowerCase().trim();
+    if(searchValue == "")
+    {
+        alert("empty search not allowed");
+        return;
+    }
+    taskContainer.innerHTML = "";
+    for(let i=0; i < tasksArray.length; i++)
+    {
+        let taskName = tasksArray[i].taskName.toLocaleLowerCase().trim();
+        if(taskName == searchValue||taskName.includes(searchValue))
+        {
+            createTaskDiv(tasksArray[i], false);
+        }
+        else
+        {
+            noTaskMsg.style.display = "block";
+        }
+    }
+    if(taskContainer.childElementCount == 0)
+    {
+        noTaskMsg.style.display == "block";
+    }
+    searchInput.value = ""
+
+});
 mainFrame.appendChild(frame2);
